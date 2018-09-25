598f2ca82bafe105049fb8b8d33ef6b22a406acb
Maya <m.hilbert@B8165-W58620.local>
2018-09-24 21:47:05 -0400

    class updates

---
+++ Assets/Scripts/Managers/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target; //the position that the camera will be following
+	public float smoothing = 5f;
+	Vector3 offset;
+
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing);
+	}
+}
